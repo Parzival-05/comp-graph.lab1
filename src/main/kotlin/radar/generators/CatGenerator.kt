871ae04752746a9a722347c3e46711b2899a4912
David <parzivalwasd@gmail.com>
2024-11-17 05:11:23 +0300

    Size of cat is fixed for now

--- src/main/kotlin/radar/generators/CatGenerator.kt
+++ src/main/kotlin/radar/generators/CatGenerator.kt
@@ -2,8 +2,6 @@ package radar.generators
 
 import GRID_SIZE_X
 import GRID_SIZE_Y
-import MAX_PARTICLE_RADIUS
-import MIN_PARTICLE_RADIUS
 import core.base.generators.BaseParticleGenerator
 import radar.scene.Offset2D
 import radar.scene.Point2D
@@ -21,11 +19,6 @@ class CatGenerator : BaseParticleGenerator<CatParticle, Point2D, Offset2D> {
     }
 
     fun generate(coordinates: Point2D): CatParticle {
-        val radius = if (MIN_PARTICLE_RADIUS != MAX_PARTICLE_RADIUS) {
-            Random.nextInt(MIN_PARTICLE_RADIUS, MAX_PARTICLE_RADIUS)
-        } else {
-            MIN_PARTICLE_RADIUS
-        }
-        return CatParticle(coordinates, radius)
+        return CatParticle(coordinates)
     }
 }
--- src/main/kotlin/radar/scene/CatParticle.kt
+++ src/main/kotlin/radar/scene/CatParticle.kt
@@ -2,7 +2,7 @@ package radar.scene
 
 import core.base.BaseParticle
 
-data class CatParticle(override var coordinates: Point2D, val radius: Int, var state: CatStates = CatStates.CALM) :
+data class CatParticle(override var coordinates: Point2D, var state: CatStates = CatStates.CALM) :
     BaseParticle<Point2D, Offset2D>() {
 
     fun setCatState(state: CatStates) {
