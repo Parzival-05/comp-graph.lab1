7e2d179900f8aa24c00c45cdd234e35f5cce2a83
David <parzivalwasd@gmail.com>
2024-11-19 01:39:26 +0300

    Add deletion of excess log recording

--- src/main/kotlin/radar/logging/logging.kt
+++ src/main/kotlin/radar/logging/logging.kt
@@ -1,5 +1,6 @@
 package radar.logging
 
+import CatSimulation.Companion.MAX_INTERACTIONS_DISPLAYED
 import androidx.compose.runtime.mutableStateListOf
 import radar.scene.CatInteraction
 import radar.scene.CatCollision
@@ -13,4 +14,7 @@ fun logging(collision: CatCollision, newState: CatStates) {
     val cat1Id = collision.particle1.id
     val cat2Id = collision.particle2.id
     GlobalInteractionLog.add(CatInteraction(time, cat1Id, cat2Id, newState))
+    if (GlobalInteractionLog.size > MAX_INTERACTIONS_DISPLAYED) {
+        GlobalInteractionLog.removeAt(0)
+    }
 }
