6404cf98d69fcd565dabe5f3040cc4362afe0f68
David <parzivalwasd@gmail.com>
2024-11-18 08:32:31 +0300

    Update SceneConfig

--- src/main/kotlin/radar/scene/SceneConfig.kt
+++ src/main/kotlin/radar/scene/SceneConfig.kt
@@ -5,10 +5,10 @@ class SceneConfig {
         enum class MetricType { EUCLIDEAN, MANHATTAN, CURVED }
     }
 
-    var maxParticleSpeed = 10
+    var maxParticleSpeed = 1
     var metric: MetricType = MetricType.EUCLIDEAN
-    var fightDist = 10 // r_0
-    var hissDist = 50 // R_0
+    var fightDist = 1 // r_0
+    var hissDist = 2 // R_0
         set(value) { // invariant: R_0 > r_0
             field = if (value < fightDist) {
                 throw RuntimeException("Hiss distance can't be less than fight distance.")
@@ -16,5 +16,4 @@ class SceneConfig {
                 value
             }
         }
-
 }
