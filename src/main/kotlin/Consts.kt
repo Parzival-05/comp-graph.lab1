7415f000e01c8178ccb7bb40bc13c319a7e61221
David <parzivalwasd@gmail.com>
2024-11-18 07:48:50 +0300

    Refactor `Consts.kt`

--- src/main/kotlin/Consts.kt
+++ src/main/kotlin/Consts.kt
@@ -1,7 +1,11 @@
-const val GRID_SIZE_X = 500
-const val GRID_SIZE_Y = 500
-const val CAT_RADIUS = 10
-const val TAU = 500L
-const val ONE_SECOND = 1000.0
-const val FRAMES_PER_TAU = (60 / (ONE_SECOND / TAU)).toInt()
-const val PARTICLE_COUNT = 50
+class CatSimulation {
+    companion object {
+        const val GRID_SIZE_X = 1920
+        const val GRID_SIZE_Y = 1080
+        const val CAT_RADIUS = 1
+        const val TAU = 500L
+        const val ONE_SECOND = 1000.0
+        const val FRAMES_PER_TAU = (60 / (ONE_SECOND / TAU)).toInt()
+        const val PARTICLE_COUNT = 50000
+    }
+}
