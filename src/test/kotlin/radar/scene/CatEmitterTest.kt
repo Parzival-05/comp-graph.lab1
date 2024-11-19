a1ea142ee87e6b9a3f53dd93289aae42e71cdb36
ancavar <danaj2004@yandex.ru>
2024-11-19 13:31:21 +0300

    test: add basic tests

---
+++ src/test/kotlin/radar/scene/CatEmitterTest.kt
@@ -0,0 +1,16 @@
+package radar.scene
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import radar.generators.CatGenerator
+
+class CatEmitterTest {
+
+    @Test
+    fun `test emit generates correct number of particles`() {
+        val generator = CatGenerator()
+        val emitter = CatEmitter(particleGenerator = generator)
+        val cats = emitter.emit(5)
+        assertEquals(5, cats.size)
+    }
+}
