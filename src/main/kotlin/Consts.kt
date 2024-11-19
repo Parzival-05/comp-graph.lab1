340896ae31a2be5e1c1a7df2df269561c49f3cea
Anna-er <brungilda364@gmail.com>
2024-11-19 03:23:56 +0300

    change some constants

--- src/main/kotlin/Consts.kt
+++ src/main/kotlin/Consts.kt
@@ -1,8 +1,7 @@
 class CatSimulation {
     companion object {
-        const val GRID_SIZE_X = 1920
-        const val GRID_SIZE_Y = 1080
-        const val CAT_RADIUS = 1
+        const val GRID_SIZE_X = 500
+        const val GRID_SIZE_Y = 500
         const val TAU = 500L
         const val PARTICLE_COUNT = 50000
         const val MAX_INTERACTIONS_DISPLAYED = 50
