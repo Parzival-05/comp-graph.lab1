4cd2d7aa51421a047bbb368765db45f5fa1b4a02
David <parzivalwasd@gmail.com>
2024-11-26 22:44:58 +0300

    Fix numeric types in unit tests

--- src/test/kotlin/radar/scene/CatParticleTest.kt
+++ src/test/kotlin/radar/scene/CatParticleTest.kt
@@ -7,21 +7,21 @@ import org.junit.jupiter.api.Test
 class CatParticleTest {
     @Test
     fun `test default state is CALM`() {
-        val cat = CatParticle(coordinates = Point2D(0f, 0f))
+        val cat = CatParticle(coordinates = Point2D(0.0, 0.0))
         assertEquals(CatStates.CALM, cat.state)
     }
 
     @Test
     fun `test setting state to HISS`() {
-        val cat = CatParticle(coordinates = Point2D(0f, 0f))
+        val cat = CatParticle(coordinates = Point2D(0.0, 0.0))
         cat.setCatState(CatStates.HISS)
         assertEquals(CatStates.HISS, cat.state)
     }
 
     @Test
     fun `test unique IDs are assigned`() {
-        val cat1 = CatParticle(coordinates = Point2D(0f, 0f))
-        val cat2 = CatParticle(coordinates = Point2D(1f, 1f))
+        val cat1 = CatParticle(coordinates = Point2D(0.0, 0.0))
+        val cat2 = CatParticle(coordinates = Point2D(1.0, 1.0))
         assertNotEquals(cat1.id, cat2.id)
     }
 }
--- src/test/kotlin/radar/scene/Point2DTest.kt
+++ src/test/kotlin/radar/scene/Point2DTest.kt
@@ -10,19 +10,19 @@ class Point2DTest {
 
     @Test
     fun `test isInScene when point is within bounds`() {
-        val point = Point2D(x = 50f, y = 50f)
+        val point = Point2D(x = 50.0, y = 50.0)
         assertTrue(point.isInScene())
     }
 
     @Test
     fun `test isInScene when point is out of bounds negative`() {
-        val point = Point2D(x = -1f, y = -1f)
+        val point = Point2D(x = -1.0, y = -1.0)
         assertFalse(point.isInScene())
     }
 
     @Test
     fun `test isInScene when point is out of bounds positive`() {
-        val point = Point2D(x = GRID_SIZE_X.toFloat() + 1, y = GRID_SIZE_Y.toFloat() + 1)
+        val point = Point2D(x = GRID_SIZE_X + 1, y = GRID_SIZE_Y + 1)
         assertFalse(point.isInScene())
     }
 }
