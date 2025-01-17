3b014c980be759b0abe754d5609f70f8a527db3e
ancavar <danaj2004@yandex.ru>
2025-01-17 15:30:02 +0300

    Add more docs to offset generators

--- src/main/kotlin/radar/generators/CircularOffsetGenerator.kt
+++ src/main/kotlin/radar/generators/CircularOffsetGenerator.kt
@@ -10,6 +10,7 @@ import kotlin.math.sin
 
 /**
  * Generates circular movement offsets for [BaseParticle] based on the given radius and angle.
+ *
  * @param radius The radius of the circle.
  */
 class CircularOffsetGenerator<P : BaseParticle<Point2D, Offset2D>>(
@@ -17,8 +18,9 @@ class CircularOffsetGenerator<P : BaseParticle<Point2D, Offset2D>>(
     private var angle: Double = 0.0,
 ) : BaseOffsetGenerator<P, Point2D, Offset2D> {
     override fun generate(particle: P): Offset2D {
-        val angularVelocity = SceneConfig.maxParticleSpeed / 100
+        val angularVelocity = SceneConfig.maxParticleSpeed / 100.0
 
+        // Mapping polar coordinates to Cartesian coordinate
         val offsetX = radius * cos(angle)
         val offsetY = radius * sin(angle)
 
--- src/main/kotlin/radar/generators/CurvedLinearOffsetGenerator.kt
+++ src/main/kotlin/radar/generators/CurvedLinearOffsetGenerator.kt
@@ -10,6 +10,12 @@ import kotlin.math.sin
 
 /**
  * Generates curved linear movement offsets for [CatParticle] based on the given velocity and curve factor.
+ *
+ * This adjustment comes from periodic sinusoidal functions (sine and cosine waves), which create smooth oscillations.
+ * These oscillations are applied independently to the X and Y directions of motion, giving the particle's path a
+ * soft, curving nature.
+ * We could think of it as creating a particle's wobble or zig-zagging motion superimposed over its broader
+ * straight-line trajectory.
  * @param linearVelocityX The linear velocity in the x-direction.
  * @param linearVelocityY The linear velocity in the y-direction.
  * @param curveFactor The curve factor for the movement.
@@ -26,8 +32,9 @@ class CurvedLinearOffsetGenerator<P : BaseParticle<Point2D, Offset2D>>(
         val curvedX = linearVelocityX + curveFactor * sin(2 * Math.PI * frequency * time)
         val curvedY = linearVelocityY + curveFactor * cos(2 * Math.PI * frequency * time)
 
-        // converting to seconds
-        time += SceneConfig.tau / 1000
+        // Increment the time value to simulate movement progression. Think of it as an x-axis, while curvedX and
+        // curvedY are some functions that depend on it and correspond to y-axis.
+        time += SceneConfig.tau / 1000.0
 
         return Offset2D(curvedX * SceneConfig.maxParticleSpeed, curvedY * SceneConfig.maxParticleSpeed)
     }
