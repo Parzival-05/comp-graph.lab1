072353bdc1f983efd412ef24b907ce8acc87983f
Anna-er <brungilda364@gmail.com>
2025-01-21 21:06:45 +0300

    Make teleportation great again

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import CatSimulation.Companion.FPS
+import CatSimulation.Companion.GRID_SIZE_X
+import CatSimulation.Companion.GRID_SIZE_Y
 import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -67,8 +69,21 @@ data class CatParticleForDraw(
         from.y += (to.y - from.y) * progress
     }
 
+
     fun updateGoal() {
-        to = cat.coordinates.copy()
+        val newCoords = cat.coordinates.copy()
+
+        // Проверка телепортации по оси X
+        if (kotlin.math.abs(newCoords.x - to.x) > GRID_SIZE_X / 2) {
+            from.x = newCoords.x // Мгновенный переход
+        }
+        // Проверка телепортации по оси Y
+        if (kotlin.math.abs(newCoords.y - to.y) > GRID_SIZE_Y / 2) {
+            from.y = newCoords.y // Мгновенный переход
+        }
+
+        // Обновляем конечные координаты
+        to = newCoords
     }
 }
 
