637753504e47adf75f5266d9f38bcf25dd6310d5
Anna-er <brungilda364@gmail.com>
2025-01-21 21:23:14 +0300

    Fix linter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 import CatSimulation.Companion.FPS
 import CatSimulation.Companion.GRID_SIZE_X
 import CatSimulation.Companion.GRID_SIZE_Y
-import androidx.compose.runtime.*
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import classes.ModelingStates
@@ -69,20 +74,17 @@ data class CatParticleForDraw(
         from.y += (to.y - from.y) * progress
     }
 
-
     fun updateGoal() {
         val newCoords = cat.coordinates.copy()
 
-        // Проверка телепортации по оси X
         if (kotlin.math.abs(newCoords.x - to.x) > GRID_SIZE_X / 2) {
             from.x = newCoords.x // Мгновенный переход
         }
-        // Проверка телепортации по оси Y
+
         if (kotlin.math.abs(newCoords.y - to.y) > GRID_SIZE_Y / 2) {
             from.y = newCoords.y // Мгновенный переход
         }
 
-        // Обновляем конечные координаты
         to = newCoords
     }
 }
--- src/main/kotlin/drawing/CatSimulationScreen.kt
+++ src/main/kotlin/drawing/CatSimulationScreen.kt
@@ -4,7 +4,12 @@ import CatParticleForDraw
 import CatSimulation.Companion.GRID_SIZE_X
 import CatSimulation.Companion.GRID_SIZE_Y
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
