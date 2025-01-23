a07d86c92ca66c1da65f1e2908e9a8dcf60e935c
ancavar <danaj2004@yandex.ru>
2025-01-23 15:29:15 +0300

    add property based testing for metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
@@ -25,6 +26,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
     testImplementation("org.mockito:mockito-core:5.1.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
+    testImplementation("net.jqwik:jqwik-kotlin:1.9.2")
 }
 
 subprojects {
@@ -36,6 +38,19 @@ subprojects {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    compilerOptions {
+        freeCompilerArgs =
+            listOf(
+                "-Xnullability-annotations=@org.jspecify.annotations:strict",
+                "-Xemit-jvm-type-annotations", // Enable annotations on type variables
+            )
+        apiVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+        languageVersion = org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0
+        javaParameters = true // Get correct parameter names in jqwik reporting
+    }
 }
 
 compose.desktop {
